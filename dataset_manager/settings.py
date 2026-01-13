71c2e5fc9040f58458204c28f5ca03bf341d37fd
Salva <chavamatias1411@gmail.com>
2026-01-13 07:47:57 -0600

    9

--- dataset_manager/settings.py
+++ dataset_manager/settings.py
@@ -130,4 +130,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
